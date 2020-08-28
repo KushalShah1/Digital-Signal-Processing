6e0d6c1ccfa4f49f73f66c12c3a160ae2c1dc3f4
Kushal Shah <kushalshah@flash.net>
2020-08-28 15:48:52 -0500

    convolved two signals and plotted

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,1 +1,9 @@
-test
+import numpy as np
+import matplotlib.pyplot as plt
+arr=np.convolve([1,1,1,1,1], [0,0,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1])
+plt.xlabel('n')
+plt.ylabel('x[n]')
+plt.title('test')
+n=np.arange(21)
+plt.stem(n, arr)
+plt.show()
